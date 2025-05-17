642e412c680d6097e455d3d9431daa08f7f351bb
Juan Navarro <juanm.navarro05@gmail.com>
2025-05-17 02:00:31 -0300

    hice un blog en django

--- ProjectoTP3/urls.py
+++ ProjectoTP3/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp import views
+# from myapp import views
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index, name="index"),
+    path("mensaje", views.index, name="index"),
 ]
